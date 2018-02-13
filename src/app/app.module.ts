edb41fa99e697135a0c90b9be223370145565514
Eddie <eddie@edease.nl>
2018-02-13 21:51:02 +0100

    easecmd first upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { RouterModule, Routes } from '@angular/router';
+
 
 import { AppComponent } from './app.component';
+import { SiteComponent } from './site/site.component';
+import { AdminComponent } from './admin/admin.component';
 
+const appRoutes: Routes = [
+  { path: 'site', component: SiteComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: '',
+  redirectTo: 'admin',
+  pathMatch: 'full'
+},
+  { path: '**', component: SiteComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SiteComponent,
+    AdminComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { useHash: true }),
   ],
   providers: [],
   bootstrap: [AppComponent]
