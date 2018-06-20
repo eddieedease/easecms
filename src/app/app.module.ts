ecab4b690973fe61be701bbf7f3aac2b3ce70af1
eddieedease <eddie@edease.nl>
2018-06-20 13:49:40 +0200

    upgradedandaddedsitestemplates

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,10 +8,14 @@ import { AppComponent } from './app.component';
 import { SiteComponent } from './site/site.component';
 import { AdminComponent } from './admin/admin.component';
 import { Site2Component } from './site2/site2.component';
+import { Site3Component } from './site3/site3.component';
+import { Site4Component } from './site4/site4.component';
 
 const appRoutes: Routes = [
   { path: 'site', component: SiteComponent },
   { path: 'site2', component: Site2Component },
+  { path: 'site3', component: Site3Component },
+  { path: 'site4', component: Site4Component },
   { path: 'admin', component: AdminComponent },
   { path: '',
   redirectTo: 'site',
@@ -25,7 +29,9 @@ const appRoutes: Routes = [
     AppComponent,
     SiteComponent,
     AdminComponent,
-    Site2Component
+    Site2Component,
+    Site3Component,
+    Site4Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/site4/site4.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-site4',
+  templateUrl: './site4.component.html',
+  styleUrls: ['./site4.component.css']
+})
+export class Site4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
